2e2375a516fbf8f6e7e65cf964fe082c38f12ad8
赵晓明 <624503860@qq.com>
2018-12-08 10:42:36 +0800

    增加目录分类

--- 拆分plist/unpack.bat
+++ 拆分plist/unpack.bat
@@ -1,11 +1,10 @@
-﻿@echo off
+@echo off
 REM 判断文件类型
 set "str=%~n1"
 SET type1=%~x1
 SET type2=".plist"
 REM 文件全名为: %~nx1, 文件名为： %~n1, 扩展名为： %~x1
 echo %str%
-
 python unpack_plist.py -plist %str%%type2% -png %str%%type1%
 pause
 exit
